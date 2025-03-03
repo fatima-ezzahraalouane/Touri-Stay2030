7c163a642f9aa02ab12b5b282fca8ef49a461b3c
fatima-ezzahraalouane <falouane38@gmail.com>
2025-03-03 10:20:35 +0000

    T2-10 T2-33 ajouter la relation avec les annonces

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,6 +17,11 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
+
+     public function annonces()
+     {
+        return $this->hasMany(Annonce::class);
+     }
     protected $fillable = [
         'name',
         'email',
